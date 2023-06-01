dd375e1a47c6c1925190de9c3d5b844e2a08a52a
Khant Sithu <khantseithuu@gmail.com>
2023-06-01 15:25:37 +0000

    feat: Build util components

---
+++ components/LikeButton.tsx
@@ -0,0 +1,18 @@
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { twMerge } from "tailwind-merge";
+
+const LikeButton = () => {
+  const isFavorite = true;
+  const Icon = isFavorite ? AiFillHeart : LikeButton;
+  return (
+    <div
+      className={twMerge(`
+      cursor-pointer 
+      hover:opacity-75 
+      transition`)}
+    >
+      <Icon color={isFavorite && "#22c55e"} size={25} />
+    </div>
+  );
+};
+export default LikeButton;
